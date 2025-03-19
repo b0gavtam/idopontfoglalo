1417b3dc23784c69d21e2349dd394b5e2bb5267a
2020b Gavrán Tamás <b0gavtam@vasvari.org>
2025-03-19 14:37:45 +0100

    még több css

--- idopontfoglalo-orai/src/views/HomeView.vue
+++ idopontfoglalo-orai/src/views/HomeView.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="container mt-5">
-    <h1 class="text-center mb-4">Időpontfoglalás - Orvosi rendelő</h1>
-    <div class="row justify-content-center">
-      <div class="col-md-6">
-        <div class="form-control">
-          <label for="idopont">Válasszon egy időpontot:&nbsp;</label>
-          <select id="idopont" v-model="valasztottId">
-            <option v-for="idopont in szabadIdopontok" :key="idopont.id" :value="idopont.id">
-              {{ idopont.datum }} - {{ idopont.ora }}
-            </option>
-          </select>
+  <div class="container d-flex justify-content-center align-items-center mt-5">
+    <div class="card p-4 shadow-lg" style="max-width: 500px;">
+      <h1 class="text-center mb-4 text-primary">-Időpontfoglalás- <br> Orvosi rendelő</h1>
+        <div class="mb-3 p-4">
+            <label for="idopont" class="form-label">Válasszon egy időpontot:</label>
+            <select id="idopont" class="form-select" v-model="valasztottId">
+              <option v-for="idopont in szabadIdopontok" :key="idopont.id" :value="idopont.id">
+                {{ idopont.datum }} - {{ idopont.ora }}
+              </option>
+            </select>
+            <br>
+            <div v-if="valasztottId">
+                <button class="btn btn-primary w-100" @click="foglalasIranyitas(valasztottId)">Foglalás</button>
+            </div>
         </div>
-          <div v-if="valasztottId">
-              <button class="btn btn-primary" @click="foglalasIranyitas(valasztottId)">Foglalás</button>
-          </div>
-      </div>
     </div>
   </div>
 </template>
+
 
 <script setup>
 import { ref, computed, onMounted } from "vue";
